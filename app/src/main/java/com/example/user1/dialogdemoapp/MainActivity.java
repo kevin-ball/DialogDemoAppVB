40ae9a843257148e93eba86044a3f01e46240915
Kevin Ball <kdb@tealworks.com>
2017-11-19 14:07:22 -0800

    Changed Quit Button onClick to be set from xml

--- app/src/main/java/com/example/user1/dialogdemoapp/MainActivity.java
+++ app/src/main/java/com/example/user1/dialogdemoapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
@@ -15,46 +14,43 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private AlertDialog.Builder builder;
     private DatePickerDialog myDatePicker;
-    private Button btnQuit;
     private TextView tvDateSelected;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvDateSelected = (TextView) findViewById(R.id.tvDateSelected);
+    }
 
-        btnQuit = (Button) findViewById(R.id.btnQuit);
-        btnQuit.setOnClickListener(new View.OnClickListener() {
+    /*
+     * onClick listener for QUIT Button - diplay AlertDialog
+     */
+    public void showAlertPickerDialog(View v) {
+        builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(getResources().getString(R.string.dialog_title));
+        builder.setMessage(getResources().getString(R.string.dialog_message));
+        builder.setCancelable(false);
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle(getResources().getString(R.string.dialog_title));
-                builder.setMessage(getResources().getString(R.string.dialog_message));
-                builder.setCancelable(false);
-                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        MainActivity.this.finish();
-                    }
-                });
-                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-
-                AlertDialog myDialog = builder.create();
-                myDialog.show();
-
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.this.finish();
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
             }
         });
 
-        tvDateSelected = (TextView) findViewById(R.id.tvDateSelected);
-
+        AlertDialog myDialog = builder.create();
+        myDialog.show();
     }
 
+    /*
+     * onClick listener for Pick a Date Button - diplay DatePickerDialog
+     */
     public void showDatePickerDialog(View v) {
         Calendar calendar = Calendar.getInstance();
         int year_x = calendar.get(Calendar.YEAR);
@@ -65,7 +61,10 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         myDatePicker.show();
     }
 
-
+    /*
+     * onDateSet Listener for DatePickerDialog
+     * (MainActivity implements DatePickerDialog.OnDateSetListener)
+     */
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
         tvDateSelected.setText("Selected date is " + (month + 1) + "/" + dayOfMonth + "/" + year);
